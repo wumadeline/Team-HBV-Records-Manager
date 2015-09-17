5256a2fcf96a291efb8bff0fd93cdd4e308c5236
MadelineWu <wumadeline@berkeley.edu>
2015-09-16 21:09:30 -0700

    Added Person and Event classes. Added basic method/functionalities. Still need to figure out how to keep track of member points

--- recordsManager.java
+++ recordsManager.java
@@ -5,8 +5,79 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class recordsManager {
+	HashMap<String, Person> members;
+	HashMap<String, Event> events;
+	
+	public recordsManager() {
+		members = new HashMap<String, Person>();
+		events = new HashMap<String, Event>();
+	}
+	
+	public void eventEntry(String name, ArrayList<String> people) {
+		if (! events.containsKey(name)) {
+			System.out.println("Event " + name + " does not exist");
+		} 
+		Event e = events.get(name);
+		for (String s: people) {
+			Person member = members.get(s);
+			e.addPerson(member);
+			member.addEventAttendance(e);
+			System.out.println(member.myName + "attended " + name);
+		}
+	}
+	
+	public class Person {
+		private String myName;
+		private int myPoints;
+		private ArrayList<Event> myEvents;
+		
+		public Person(String name) {
+			myName = name;
+			myPoints = 0;
+			myEvents = new ArrayList<Event>();
+			members.put(myName, this);
+			System.out.println("Added new member: " + myName + "!");
+		}
+		
+		public void addEventAttendance(Event e) {
+			myEvents.add(e);
+			// THIS CHECK NEEDS TO BE FIXED
+			if ((! e.myType.equals("Other Social")) && (! e.myType.equals("General Meeting"))) {
+				this.myPoints++;
+			}
+			this.myPoints++;
+		}
+		
+		public ArrayList getEvents() {
+			return myEvents;
+		}
+	}
+	
+	public class Event {
+		private String myName; 
+		private ArrayList<Person> myPeople;
+		private String myType;
+		
+		public Event(String name, String type) {
+			myName = name;
+			myPeople = new ArrayList<Person>(); 
+			myType = type;
+			events.put(myName, this);
+			System.out.println("Created event: " + myName + "!");
+		}
+		
+		public void addPerson(Person p) {
+			if (! myPeople.contains(p)) {
+				myPeople.add(p);
+			} else {
+				System.out.println(p.myName + " is already associated with event: " + myName);
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 		recordsManager state = new recordsManager();
@@ -28,6 +99,8 @@ public class recordsManager {
 				e.printStackTrace();
 			}
 		}
+		
+		
 		
 		// load out finished .ser file
 		// following code referenced from http://avajava.com/tutorials/lessons/how-do-i-write-an-object-to-a-file-and-read-it-back.html
